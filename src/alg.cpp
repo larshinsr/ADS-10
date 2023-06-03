218d7f6064f90acb83b3d9f19ee486a5d15ee8fb
larshinsr <82053310+larshinsr@users.noreply.github.com>
2023-06-04 00:59:46 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,10 @@
 // Copyright 2022 NNTU-CS
-#include  <iostream>
-#include  <fstream>
-#include  <locale>
-#include  <cstdlib>
-#include  "tree.h"
-
+#include <iostream>
+#include <fstream>
+#include <locale>
+#include <cstdlib>
+#include "tree.h"
+#include <vector>
 std::vector<char> getPerm(const Tree& tree, int n) {
   // напишите реализацию
     std::vector<char> result;
