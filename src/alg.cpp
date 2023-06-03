ffa54b2b88d6152c6245a596e02395b13b30bfc8
larshinsr <82053310+larshinsr@users.noreply.github.com>
2023-06-04 01:41:24 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,11 +6,15 @@
 #include "tree.h"
 #include <vector>
 std::vector<char> getPerm(const Tree& tree, int n) {
-  // напишите реализацию
     std::vector<char> result;
-    result = tree.permutations[n - 1];
-    reverse(result.begin(), result.end());
-    result.pop_back();
-    reverse(result.begin(), result.end());
+    if (tree.permutations.size() < n ) {
+       result.push_back('1');
+       result.clear();
+    } else {
+        result = tree.permutations[n - 1];
+        reverse(result.begin(), result.end());
+        result.pop_back();
+        reverse(result.begin(), result.end());
+    }
     return result;
 }
